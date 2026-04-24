11cbb7f56e255c959575b4f1a0789f36cc6a585b
MH-1294 <dmabid2050@gmail.com>
2026-04-24 22:45:36 +0000

    Deploying to gh-pages from @ MH-1294/abid@7ef51fb7409076639cf8fcb31f7b1727c7cadac6 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,6 +16,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/abid/publications/";
           },
+        },{id: "nav-projects",
+          title: "projects",
+          description: "Selected research and applied projects.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/abid/projects/";
+          },
         },{id: "nav-cv",
           title: "cv",
           description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
