5afdb9ceb478911eb6a32450236d3a1aa5558993
MH-1294 <dmabid2050@gmail.com>
2026-04-24 22:21:21 +0000

    Deploying to gh-pages from @ MH-1294/abid@4d937829561fcbceb12372369e233259b9a4a57a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -438,6 +438,11 @@ ninja.data = [{
           description: "Experimental machine learning pipelines supporting research development.",
           section: "Projects",handler: () => {
               window.location.href = "/abid/projects/3_project/";
+            },},{id: "projects-project-7",
+          title: 'project 7',
+          description: "with background image",
+          section: "Projects",handler: () => {
+              window.location.href = "/abid/projects/7_project/";
             },},{
         id: 'social-linkedin',
         title: 'LinkedIn',
