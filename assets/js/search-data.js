de26aff56b9babed4f81feffec144133188c6953
MH-1294 <dmabid2050@gmail.com>
2026-04-24 22:21:04 +0000

    Deploying to gh-pages from @ MH-1294/abid@38629e5c39422eb361922433fbc3fe54543d22b5 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -448,16 +448,6 @@ ninja.data = [{
           description: "with background image",
           section: "Projects",handler: () => {
               window.location.href = "/abid/projects/7_project/";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/abid/projects/8_project/";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/abid/projects/9_project/";
             },},{
         id: 'social-linkedin',
         title: 'LinkedIn',
